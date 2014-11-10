ccb0de22bd70a822284e1b94a0183cf543bef184
aviau <alexandre.viau@savoirfairelinux.com>
2014-11-10 10:41:17 -0500

    PasswordStore: Added test_init

--- pypass/tests/test_passwordstore.py
+++ pypass/tests/test_passwordstore.py
@@ -16,3 +16,29 @@
 #    You should have received a copy of the GNU General Public License
 #    along with python-pass.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+import unittest
+import os
+import shutil
+import tempfile
+
+from pypass.passwordstore import PasswordStore
+
+
+class TestPasswordStore(unittest.TestCase):
+
+    def setUp(self):
+        self.dir = tempfile.mkdtemp()
+
+        # .gpg_id file
+        with open(os.path.join(self.dir, '.gpg-id'), 'w') as gpg_id_file:
+            gpg_id_file.write('5C5833E3')
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
+
+    def test_init(self):
+        store = PasswordStore(self.dir)
+        self.assertEqual(store.gpg_id, '5C5833E3')
+        self.assertFalse(store.uses_git)
+        self.assertEqual(self.dir, store.path)
